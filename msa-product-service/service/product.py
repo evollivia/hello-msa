664779fe62ce5baaafbed82c2798c481f1e8b585
evollivia <ytafxt@naver.com>
2024-10-21 11:20:34 +0900

    product service -  상품 수정 기능 추가

--- msa-product-service/service/product.py
+++ msa-product-service/service/product.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from models.product import Product
 from schema.product import ProductBase
 
+
 # 회원가입 처리
 # 기본 회원정보 + 번호,가입일
 def register(db: Session, product: ProductBase):
@@ -14,23 +15,21 @@ def register(db: Session, product: ProductBase):
 
     return product
 
+
 def productlist(db: Session):
-    return db.query(Product.name, Product.price,
-              Product.regdate, Product.pno)\
-              .order_by(Product.pno.desc()).all()
+    return db.query(Product.name, Product.price,Product.regdate, Product.pno) \
+        .order_by(Product.pno.desc()).all()
 
 
 def productone(db: Session, pno: int):
-    return db.query(Product)\
-            .filter(Product.pno == pno).first() \
+    return db.query(Product).filter(Product.pno == pno).first()
 
 
 def productdelete(db: Session, pno: int):
     # 삭제할 상품 조회
-    product = db.query(Product) \
-        .filter(Product.pno == pno).first()
+    product = db.query(Product).filter(Product.pno == pno).first()
 
-    if product: # 삭제할 상품이 존재한다면
+    if product:  # 삭제할 상품이 존재한다면
         db.delete(product)
         db.commit()
     else:
@@ -39,4 +38,30 @@ def productdelete(db: Session, pno: int):
     # 삭제한 상품 수를 직접 return함
     # 만일, 프로그래밍으로 삭제한 상품 수를 return 하려면
     # core orm을 이용할 것(db.execute(delete~))
+    return 1
+
+
+def productupdate(db: Session, product: Product):
+    # 삭제할 상품 조회
+    productone = db.query(Product) \
+        .filter(Product.pno == product.pno).first()
+
+    # setattr(eotkd, 키, 값) : 실행중에 특정 객체의 키를 기준으로 값을 수정함
+    # 특정 객체의 속성들을 반복적으로 처리할 때 주로 사용
+    if productone:  # 수정할 상품이 존재한다면
+        # productone.name = product.name
+        # productone.desc = product.desc
+        # productone.price = product.price
+        # productone.maker = product.maker
+        # productone.regdate = product.regdate
+
+        # 반복문으로 구현
+        for key, val in product.__dict__.items():
+            if key != 'pno' and val is not None:
+                setattr(productone, key, val)
+        db.commit()
+    else:
+        return None
+
+    # 수정한 상품 수를 직접 return함
     return 1
